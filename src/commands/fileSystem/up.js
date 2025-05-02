341f876bd3d422d196771e0844c9ebc2eb701e52
Olha Popova <fc.odessa.10@gmail.com>
2025-05-01 19:59:06 -0700

    feat: add goUpFromCurrentDirectory function for moving up one directory level

---
+++ src/commands/fileSystem/up.js
@@ -0,0 +1,13 @@
+import { chdir } from 'node:process'
+ import showCurrentDirectoryMessage from '../../utils/showCurrentDirectoryMessage'
+ 
+ const goUpFromCurrentDirectory = () => {
+ 	try {
+ 		chdir('..')
+ 		showCurrentDirectoryMessage()
+ 	} catch (err) {
+ 		console.error('Operation failed')
+ 	}
+ }
+ 
+ export default goUpFromCurrentDirectory
