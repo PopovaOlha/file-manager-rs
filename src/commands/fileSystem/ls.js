08d5ca38f01618642282d23b2c51738dae432d89
Olha Popova <fc.odessa.10@gmail.com>
2025-05-02 14:35:06 -0700

    feat: implemented ls command to list directory contents

---
+++ src/commands/fileSystem/ls.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+ import showCurrentDirectoryMessage from './showCurrentDirectoryMessage.js'
+ 
+ const listDirectoryContents = () => {
+ 	try {
+ 		const currentDir = process.cwd()
+ 		const contents = fs.readdirSync(currentDir, { withFileTypes: true })
+ 
+ 		const formattedContents = contents.map((dirent, index) => ({
+ 			index,
+ 			name: dirent.name,
+ 			type: dirent.isDirectory() ? 'directory' : 'file',
+ 		}))
+ 
+ 		formattedContents.sort((a, b) => {
+ 			if (a.type === b.type) return a.name.localeCompare(b.name)
+ 			return a.type === 'directory' ? -1 : 1
+ 		})
+ 
+ 		console.table(formattedContents)
+ 	} catch (err) {
+ 		console.error('Operation failed')
+ 	} finally {
+ 		showCurrentDirectoryMessage()
+ 	}
+ }
+ 
+ export default listDirectoryContents
