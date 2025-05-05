aedc4e0d29ff169c49000ff9807d8418cbf9ea16
Olha Popova <fc.odessa.10@gmail.com>
2025-05-04 22:04:38 -0700

    feat: implement deleteFile function to remove files

---
+++ src/commands/fileSystem/rm.js
@@ -0,0 +1,24 @@
+import { F_OK } from 'node:fs'
+ import { access, rm } from 'node:fs/promises'
+ 
+ const deleteFile = async fileRemove => {
+ 	try {
+ 		const pathRemoveFile = fileRemove.replaceAll(`"`, '')
+ 		const isFileDeleteExists = await access(pathRemoveFile, F_OK)
+ 			.then(() => {
+ 				return true
+ 			})
+ 			.catch(() => {
+ 				console.log('The file being deleted does not exist.')
+ 				return false
+ 			})
+ 		if (isFileDeleteExists) {
+ 			await rm(pathRemoveFile)
+ 			console.log(`The file has been delete from "${pathRemoveFile}"!`)
+ 		}
+ 	} catch (err) {
+ 		return
+ 	}
+ }
+ 
+ export default deleteFile
